56610e507d8d6d7573516463e068787daf978e00
Brent-Milan <Bmilan24@gmail.com>
2017-08-19 20:05:36 -0400

    card class override and rankAsString implemented, etc

--- src/main/java/production/poker/Card.java
+++ src/main/java/production/poker/Card.java
@@ -2,9 +2,9 @@ package production.poker;
 
 public class Card {
 
-	private String[] suits = { "Hearts", "Clubs", "Spades", "Diamonds" };
+	private static String[] suits = { "Hearts", "Clubs", "Spades", "Diamonds" };
 	
-	private String[] values = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King" };
+	private static String[] ranks = { "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King" };
 	
 	
 	//these could probably be 'short', but it is ugly to type over and over
@@ -12,11 +12,19 @@ public class Card {
 	
 	private int suit;
 	
+	//constructor
 	public Card(int suit, int rank ) {
 		this.suit = suit;
 		this.rank = rank;
 	}
 	
+	@Override
+	public String toString( ) {
+		return ranks[rank] + " of " + suits[suit];
+	}
 	
+	public static String rankAsString(int rank) {
+		return ranks[rank];
+	}
 	
 } //end class
